c1456beda572c0453bcaf246c65787c4dafe9e76
Ben Moore <ben.moore@replgroup.com>
2020-03-04 14:00:44 +0000

    Update to dependices

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+
+export const Header = () => <header>This is the header!</header>
+
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Header } from '../components/Header/Header';
+
+class App extends Component {
+	state = {
+		loading: false,
+		error: false,
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Header />
+			</div>
+		);
+	};
 }
 
 export default App;
